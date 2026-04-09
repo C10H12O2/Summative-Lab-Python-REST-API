afc8cb6ee82195d3b70505f344c0dc8e2fdd39f3
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-04-09 19:37:03 +0300

    Added choices for the user to choose from for the update_item function

--- cli.py
+++ cli.py
@@ -96,6 +96,62 @@ def add_item():
     except requests.exceptions.ConnectionError:
         print("\n [Error] Could not connect to the server, make sure app.py is running.")
         
+ def update_item():
+     try:
+         item_id = int(input("\n Enter item ID to update: "))
+        except ValueError:
+            print(" [Error] Invalid ID, please enter a number.") 
+            return
         
+        print("\n What would you like to update?")
+        print(" 1. Product Name")
+        print(" 2. Brand")
+        print(" 3. Category")
+        print(" 4. Price")
+        print(" 5. Quantity")
+        print(" 6. Expiration Date")
+        print(" 7. Barcode")
+        print(" 8. Ingredients")
+        choice = input(" Enter choice (1-8): ").strip()
+        
+        try:
+            if choice == "1":
+                payload = {"product_name": input(" New Product Name: ").strip()}
+            elif choice == "2":
+                payload = {"brands": input(" New Brand: ").strip()}
+            elif choice == "3":
+                payload = {"category": input(" New Category: ").strip()}
+            elif choice == "4":
+                payload = {"price": float(input(" New Price: ").strip())}
+            elif choice == "5":
+                payload = {"quantity": int(input(" New Quantity: ").strip())}
+            elif choice == "6":
+                payload = {"expiration_date": input(" New Expiration Date (YYYY-MM-DD): ").strip()}
+            elif choice == "7":
+                payload = {"barcode": input(" New Barcode: ").strip()}
+            elif choice == "8":
+                payload = {"ingredients_text": input(" New Ingredients: ").strip()}
+                
+            else:
+                print(" [Error] Invalid choice.")
+                return
+        except ValueError:
+            print(" [Error] Invalid input for price or quantity.")
+            return
+        
+        try:
+            response = requests.patch(f"{BASE_URL}/inventory/{item_id}", json=payload)
+            data = response.json()
+            
+            if response.status_code == 200:
+                print(f"\n Item updated successfully!")
+                print_item(data["item"])
+            else:
+                print(f"\n [Error] {data.get('message')}")
+        
+        except requests.exceptions.ConnectionError:
+            print("\n [Error] Could not connect to the server, make sure app.py is running.")
+            
+            
         
             
